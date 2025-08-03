9e9a555cc4ab3e350cfa95f021a9a31b12fdf65d
João Pombeiro <joaodss11@gmail.com>
2025-08-03 21:43:13 +0100

    feat(products): add products on BE
    setup graphql API for products

---
+++ apps/pet-market-be/prisma/migrations/20250803192258_initial_setup_test/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - The primary key for the `Product` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."OrderItem" DROP CONSTRAINT "OrderItem_productId_fkey";
+
+-- AlterTable
+ALTER TABLE "public"."OrderItem" ALTER COLUMN "productId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Product" DROP CONSTRAINT "Product_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Product_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "public"."OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
