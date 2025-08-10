f352d4b8e041b40f9381a8a35f0eb13fbb4ed8c0
João Pombeiro <joaodss11@gmail.com>
2025-08-10 12:07:02 +0100

    Add the cart icon and implement the cart page

---
+++ apps/pet-market-web/src/app/cart/cart.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartStore } from '../stores/cart.store';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cart',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './cart.html',
+  styleUrl: './cart.scss',
+})
+export class Cart {
+  cartStore = inject(CartStore);
+
+  updateQuantity(productId: string, event: Event) {
+    const target  = event.target as HTMLInputElement;
+    const quantity = parseInt(target.value);
+    if ( quantity > 0) {
+      this.cartStore.updateQuantity(productId, quantity)
+    }
+  }
+}
